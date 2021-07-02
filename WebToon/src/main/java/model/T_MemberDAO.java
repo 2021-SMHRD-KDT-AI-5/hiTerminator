ca3bbd53848fa56b3bc403c644b4c91c012678d3
SM2129 <SM2129@DESKTOP-R20JL7U>
2021-07-02 14:16:10 +0900

    D_member Dao, dDto 만듦

--- WebToon/src/main/java/model/T_MemberDAO.java
+++ WebToon/src/main/java/model/T_MemberDAO.java
@@ -1,5 +1,130 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class T_MemberDAO {
+
+	private Connection conn = null;
+	private PreparedStatement psmt = null;
+	private ResultSet rs = null;
+
+	public void connection() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// localhost : Oracle DB가 설치된 PC의 ip주소 설정
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String user = "hr";
+			String pass = "hr";
+
+			conn = DriverManager.getConnection(url, user, pass);
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("ojdbc6.jar파일 또는 driver경로 확인");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("DB연결실패 또는 SQL문 오류");
+			e.printStackTrace();
+		}
+
+	}
+
+	public void close() {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+
+			if (psmt != null) {
+				psmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	   public int insert_member(T_MemberDTO member) {
+		   	// 회원가입 메소드
+
+		      int cnt = 0;
+
+		      try {
+		         connection();
+		         // 쿼리 실행
+		         String sql = "insert into book_member values(?,?,?,?,?)";
+
+		         psmt = conn.prepareStatement(sql);
+
+		         psmt.setString(1, member.getMember_id());
+		         psmt.setString(2, member.getPw());
+		         psmt.setString(3, member.getName());
+		         psmt.setString(4, member.getTel());
+		         psmt.setString(5, member.getEmail());
+
+		         cnt = psmt.executeUpdate();
+
+		      } catch (SQLException e) {
+		         // SQL문 관련오류 발생시 실행되는 캣치-문
+		         System.out.println("SQL문 에러");
+		         e.printStackTrace();
+		      } finally {
+		         // 무조건 실행하는 구문
+		         // 데이터베이스 종료기능 구현
+		         close();
+		      } // end
+		      return cnt;
+
+		   }
+
+		   public T_MemberDTO login_member(String id, String pw) {
+			   // 로그인 메소드
+
+		      T_MemberDTO member = null;
+
+		      try {
+		         // DB연결기능
+		         connection();
+
+		         // 쿼리 실행
+		         String sql = "select * from book_member where id=? and pw=?";
+
+		         psmt = conn.prepareStatement(sql);
+
+		         psmt.setString(1, id);
+		         psmt.setString(2, pw);
+
+		         // insert, update, delete : executeUpdate()
+		         // select : executeQuery
+		         rs = psmt.executeQuery();
+
+		         // next(): 다음 행으로 넘어가서 데이터 존재 여부 판단(true/false)
+		         if (rs.next()) {// select한 데이터가 있다면
+		            String getId = rs.getString(1);
+		            String getNick = rs.getString(3);
+		            String getEmail = rs.getString(4);
+		            String getTel = rs.getString(5);
+
+		            member = new T_MemberDTO(getId, null, getNick, getEmail, getTel);
+		         }
+
+		      } catch (SQLException e) {
+		         System.out.println("sql문 에러");
+		         e.printStackTrace();
+		      } finally {
+		         // 무조건 실행하는 구문
+		         // 데이터베이스 종료기능 구현
+		         close();
+		      } // end
+		      return member;
+		   }
+	
 
 }
--- WebToon/src/main/java/model/T_MemberDTO.java
+++ WebToon/src/main/java/model/T_MemberDTO.java
@@ -1,5 +1,66 @@
 package model;
 
 public class T_MemberDTO {
+	private String Member_id;
+	private String pw;
+	private String name;
+	private String tel;
+	private String email;
+	// ck -> 累啊咯何, 包府磊 咯何
+	
+	// 积己磊
+	public T_MemberDTO(String member_id, String pw, String name, String tel, String email) {
+		this.Member_id = member_id;
+		this.pw = pw;
+		this.name = name;
+		this.tel = tel;
+		this.email = email;
+	}
+	
+	// getter setter
 
+	public String getMember_id() {
+		return Member_id;
+	}
+
+	public void setMember_id(String member_id) {
+		Member_id = member_id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+	
 }
