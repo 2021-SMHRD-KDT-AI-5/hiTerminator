ca5569c8eee4190d60fa120df4f07eb0c39e3be2
SM2105 <SM2105@DESKTOP-ACM9K6A>
2021-07-06 09:14:10 +0900

    admin service.jsp

--- WebToon/src/main/java/model/Consulting_messageDAO.java
+++ WebToon/src/main/java/model/Consulting_messageDAO.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Consulting_messageDAO {
 	
@@ -49,5 +50,71 @@ public class Consulting_messageDAO {
 
 	}
 
-	
+	public int insertMessage(Consulting_messageDTO message) {
+
+		int cnt = 0;
+
+		try {
+			connection();
+			// 쿼리 실행
+			String sql = "insert into consulting_message values(num_message.nextval,?,?,?,sysdate)";
+			// num_message.nextval -> 시퀀스 값
+			// sysdate -> 시스템 날짜
+
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, message.getMember_id());
+			psmt.setString(2, message.getTitle());
+			psmt.setString(3, message.getConsult_content());
+
+			// insert, update, delete : executeUpdate() --> DB내용 변경
+			// select : executeQuery() --> DB내용 검색
+			cnt = psmt.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println("sql문 오류");
+			e.printStackTrace();
+
+		} finally {
+			close();
+		} // end
+
+		return cnt;
+
+	}
+
+	public ArrayList<Consulting_messageDTO> showMessage(String member_id) {
+
+		ArrayList<Consulting_messageDTO> list = new ArrayList<Consulting_messageDTO>();
+		Consulting_messageDTO message = null;
+
+		try {
+			// DB연결기능
+			connection();
+
+			String sql = "select * from consulting_message where member_id = ?";
+
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, member_id);
+
+			rs = psmt.executeQuery();
+
+			while (rs.next()) {
+				int getNum = rs.getInt(1);
+				String getMember_id = rs.getString(2);
+				String getTitle = rs.getString(3);
+				String getConsult_content = rs.getString(4);
+				String getApply_date = rs.getString(4);
+
+				message = new Consulting_messageDTO(getNum, getMember_id, getTitle, getConsult_content, getApply_date);
+				list.add(message);
+			}
+		} catch (SQLException e) {
+			System.out.println("sql오류");
+			e.printStackTrace();
+		} finally {
+			close();
+		} // end
+
+		return list;
+	}
 }
--- WebToon/src/main/java/model/Consulting_messageDTO.java
+++ WebToon/src/main/java/model/Consulting_messageDTO.java
@@ -1,58 +1,67 @@
 package model;
 
 public class Consulting_messageDTO {
+	
+	// 메세지 번호, 회원 아이디, 보낸날짜, 제목, 첨부파일 
+	
+	private int num;
 	private String member_id;
-	private String apply_date;
 	private String title;
 	private String consult_content;
+	private String apply_date;
 	
-	
-	public Consulting_messageDTO(String member_id, String apply_date, String title, String consult_content) {
-		super();
+	public Consulting_messageDTO(int num, String member_id, String title, String consult_content, String apply_date) {
+		this.num = num;
 		this.member_id = member_id;
+		this.title = title;
+		this.consult_content = consult_content;
 		this.apply_date = apply_date;
+	}
+	
+	public Consulting_messageDTO(String member_id, String title, String consult_content) {
+		this.member_id = member_id;
 		this.title = title;
 		this.consult_content = consult_content;
 	}
 
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
 
 	public String getMember_id() {
 		return member_id;
 	}
 
-
 	public void setMember_id(String member_id) {
 		this.member_id = member_id;
 	}
-
-
-	public String getApply_date() {
-		return apply_date;
-	}
-
-
-	public void setApply_date(String apply_date) {
-		this.apply_date = apply_date;
-	}
-
 
 	public String getTitle() {
 		return title;
 	}
 
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 
 	public String getConsult_content() {
 		return consult_content;
 	}
 
-
 	public void setConsult_content(String consult_content) {
 		this.consult_content = consult_content;
+	}
+
+	public String getApply_date() {
+		return apply_date;
+	}
+
+	public void setApply_date(String apply_date) {
+		this.apply_date = apply_date;
 	}
 	
 }
